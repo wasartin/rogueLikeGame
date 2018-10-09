c47acb01170aa3a11d708ea6b6171688a22362a9
wasartin <wasartin@iastate.edu>
2018-10-09 09:02:31 -0500

    breaking npc/pc into their own files

---
+++ character.h
@@ -0,0 +1,12 @@
+#ifndef CHARACTER_H
+# define CHARACTER_H
+
+#include "dungeonInfo.h"
+
+void makeMonsters(Dungeon *d);
+void placeMonsters(Dungeon *d);
+int isInSameRoom(Dungeon *d, Character *monster);
+void moveMonster(Dungeon *d, Character *monster);
+void moveCharacter(Dungeon *d, int row, int col, Character *curr);
+
+#endif
